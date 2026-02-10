f50514de756112e4f86909b22df36c03688551d8
Israel_Winnett <arulik98@gmail.com>
2026-02-10 18:03:11 +0200

    completed profile

---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='text-6xl text-center p-5 border-2 bg-black text-cyan-50 font-bold'>My Profile Page</header>
+  )
+}
